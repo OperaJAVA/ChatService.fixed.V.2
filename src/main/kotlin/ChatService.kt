1217ce5c8243bbebfc6bc2ea2068135904e4b7d6
OperaJAVA <cassandrabk478@gmail.com>
2023-12-25 17:09:14 +0300

    first commit

--- src/main/kotlin/ChatService.kt
+++ src/main/kotlin/ChatService.kt
@@ -1,71 +1,81 @@
 package ru.netology
-import java.util.*
 
+// Класс, представляющий сервис работы с чатами
 class ChatService {
-    val chats = hashMapOf<SortedSet<Int>, Chat>()
+    val chats = hashMapOf<Set<Int>, Chat>()
 
     override fun toString(): String {
-        var result: String = "Состояние чатов:\n"
-        chats.onEach { (key, value) ->
+        var result = "Состояние чатов:\n"
+        chats.forEach { (key, value) ->
             result += "$key $value \n"
         }
         return result
     }
 
+    // Метод для добавления сообщения в чат
     fun addMessage(userIds: List<Int>, message: Message): Chat {
-        return chats.getOrPut(userIds.toSortedSet()) {
+        return chats.getOrPut(userIds.toSet()) {
             Chat(null)
         }.apply {
             messages.add(message)
         }
     }
 
-    fun getMessagesFromChat(userIds: SortedSet<Int>): List<Message> {
-        val chat = chats.filter { entry -> entry.key.containsAll(userIds) }.values.first().messages
-        chat.onEach { message -> message.isRead = true }
-        return chat
+    // Метод для получения сообщений из чата по идентификатору пользователей
+    fun getMessagesFromChat(userIds: Set<Int>): List<Message> {
+        val chat = chats.filter { entry -> entry.key.containsAll(userIds) }.values.firstOrNull()
+        chat?.messages?.forEach { message -> message.isRead = true }
+        return chat?.messages ?: emptyList()
     }
 
+    // Метод для пометки сообщений в чате как прочитанных
     fun readChat(userIds: List<Int>): Boolean {
-        chats.filter { entry -> entry.key.containsAll(userIds) }.values.first().readMessages()
+        val chat = chats.filter { entry -> entry.key.containsAll(userIds) }.values.firstOrNull()
+        chat?.readMessages()
         return true
     }
 
+    // Метод для удаления чата по идентификатору пользователей
     fun deleteChat(userIds: List<Int>): Boolean {
-        return chats.remove(userIds.toSortedSet()) != null
+        return chats.remove(userIds.toSet()) != null
     }
 
+    // Метод для получения всех чатов пользователя
     fun getChats(userId: Int): List<Chat> {
         return chats.filter { entry -> entry.key.contains(userId) }.values.toList()
     }
 
+    // Метод для получения чата по идентификатору пользователей
     fun getChat(userIds: List<Int>): List<Chat> {
         return chats.filter { entry -> entry.key.containsAll(userIds) }.values.toList()
     }
 
+    // Метод для получения количества непрочитанных чатов у пользователя
     fun getUnreadChatsCount(userId: Int): Int {
-        return chats.filter { entry -> entry.key.contains(userId) }.values.filter { !it.unreadMessagesB() }.count()
+        return chats.filter { entry -> entry.key.contains(userId) }.values.filter { !it.hasUnreadMessages() }.count()
     }
 
-    fun getLastMessages(chatIds: List<SortedSet<Int>>): List<String> {
+    // Метод для получения последних сообщений из чатов
+    fun getLastMessages(chatIds: List<Set<Int>>): List<String> {
         return chatIds.flatMap { chatId ->
-            val MESSAGE_LIMIT = 10
+            val messageLimit = 10
             getMessagesFromChat(chatId)
                 .reversed()
-                .take(MESSAGE_LIMIT)
-                .map { message ->
+                .take(messageLimit)
+                .mapNotNull { message ->
                     message.text
-                }.filterNotNull()
+                }
         }
     }
 
+    // Метод для пометки сообщений в чате как прочитанных по идентификатору пользователя и количеству сообщений
     fun readMessagesFromChatById(userId: Int, messageCount: Int) {
         chats
             .filter { it.value.userId == userId }
-            .onEach { chat ->
+            .forEach { chat ->
                 getMessagesFromChat(chat.key)
                     .take(messageCount)
-                    .onEach {
+                    .forEach {
                         it.isRead = true
                     }
             }
